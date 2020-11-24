9fb5b6123ba560cf35bfc69e0b12b1aa7b9c7780
Svetlana Khaustova <svetlanka.ender@yandex.ru>
2020-11-24 18:02:16 +0200

    исправоен скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
-let popup = document.querySelector('.popup');
-let popupCloseButton = document.querySelector('.popup__close');
-let editButton = document.querySelector('.profile__edit-button');
-let firstName = document.querySelector('.profile__first-name');
-let lastName = document.querySelector('.profile__last-name');
-let form = document.querySelector('.popup__form');
-let nameFieled = document.querySelector('.popup__input_type_name');
-let lastFieles = document.querySelector('.popup__input_type_title');
-let saveButton = document.querySelector('.popup__save');
+const popup = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close');
+const editButton = document.querySelector('.profile__edit-button');
+const firstName = document.querySelector('.profile__first-name');
+const lastName = document.querySelector('.profile__last-name');
+const form = document.querySelector('.popup__form');
+const nameFieled = document.querySelector('.popup__input_type_name');
+const lastFieles = document.querySelector('.popup__input_type_title');
+const saveButton = document.querySelector('.popup__save');
 
 const initialCards = [
     {
@@ -35,24 +35,29 @@ const initialCards = [
     }
 ]; 
 
-function showPopup () {
-    popup.classList.add('popup_opened');
+function showPopup (popupAll) {
+    popupAll.classList.add('popup_opened');
+}
+
+function closePopup (popupAll) {
+    popupAll.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', function() {
+    showPopup(popup);
     nameFieled.value = firstName.textContent;
     lastFieles.value = lastName.textContent;
-}
+});
 
-function closePopup () {
-    popup.classList.remove('popup_opened');
-}
-
-editButton.addEventListener('click', showPopup);
-popupCloseButton.addEventListener('click', closePopup);
+popupCloseButton.addEventListener('click', function() {
+    closePopup(popup);
+});
 
 function submitForm (event) {
     event.preventDefault();
     firstName.textContent = nameFieled.value;
     lastName.textContent = lastFieles.value;
-    closePopup();
+    closePopup(popup);
 }
 
 form.addEventListener('submit', submitForm);
@@ -63,80 +68,73 @@ const mestoTemplate = document.querySelector('.grid-tamplate').content;
 const popupPhoto = document.querySelector('.popup-photo');
 const closePhoto = document.querySelector('.popup-photo__close')
 
-function addMesto(item) {
+function createCard(name, link) {
     const mestoElement = mestoTemplate.cloneNode(true);
     
-    mestoElement.querySelector('.grid__name').textContent = item.name;
-    mestoElement.querySelector('.grid__photo').src = item.link;
+    mestoElement.querySelector('.grid__name').textContent = name;
+    mestoElement.querySelector('.grid__photo').src = link;
 
     const mestoDelete = mestoElement.querySelector('.grid__delete');
-
     mestoDelete.addEventListener('click', event => {
-        const mesto = event.target.closest('.grid__element');
-
-        if (mesto) {
-            mesto.remove()
-        }
+        event.target.closest('.grid__element').remove()
     });
     
     const likeButton = mestoElement.querySelector('.grid__like-button');
-
     likeButton.addEventListener('click', function() {
         likeButton.classList.toggle('grid__button_active');
     });
 
     const mestoPhoto = mestoElement.querySelector('.grid__photo');
-
     mestoPhoto.addEventListener('click', function() {
-        popupPhoto.classList.add('popup-photo_active');
+        showPopup(popupPhoto);
         const photo = document.querySelector('.popup-photo__photo');
-        photo.src = item.link;
+        photo.src = link;
         const name = document.querySelector('.popup-photo__name');
-        name.textContent = item.name;
+        name.textContent = name;
     });
 
-    mestoList.prepend(mestoElement);
+    return mestoElement;
 } 
 
+function addCard(container, cardElement) {
+    container.prepend(cardElement);
+}
+
+initialCards.forEach((value) => addCard(mestoList, createCard(value.name, value.link)));
+
 closePhoto.addEventListener('click', function() {
-    popupPhoto.classList.remove('popup-photo_active');
+    closePopup(popupPhoto);
+    formAdd.reset();
 });
 
+const popupAdd = document.querySelector('.popup-add');
+const popupAddCloseButton = document.querySelector('.popup-add__close');
+const addPopupButton = document.querySelector('.profile__add-button');
 
-initialCards.forEach(addMesto);
+addPopupButton.addEventListener('click', function() {
+    showPopup(popupAdd);
+});
 
-let popupAdd = document.querySelector('.popup-add');
-let popupAddCloseButton = document.querySelector('.popup-add__close');
-let addPopupButton = document.querySelector('.profile__add-button');
+popupAddCloseButton.addEventListener('click', function() {
+    closePopup(popupAdd);
+    formAdd.reset();
+});
 
-function showPopupAdd () {
-    popupAdd.classList.add('popup-add_opened');
-}
-
-function closePopupAdd () {
-    popupAdd.classList.remove('popup-add_opened');
-    nameAdd.value = null;
-    photoAdd.value = null;
-}
-
-addPopupButton.addEventListener('click', showPopupAdd);
-popupAddCloseButton.addEventListener('click', closePopupAdd);
-
-let formAdd = document.querySelector('.popup-add__form');
-let nameAdd = document.querySelector('.popup-add__input_type_name');
-let photoAdd = document.querySelector('.popup-add__input_type_photo');
+const formAdd = document.querySelector('.popup-add__form');
+const nameAdd = document.querySelector('.popup-add__input_type_name');
+const photoAdd = document.querySelector('.popup-add__input_type_photo');
 
 function submitFormAdd (event) {
     event.preventDefault();
-    
+
     let item = {
         name: nameAdd.value,
         link: photoAdd.value
     }
 
-    addMesto(item);
-
-    closePopupAdd();
+    addCard(mestoList, createCard(item.name, item.link));
+    closePopup(popupAdd);
+    formAdd.reset();
 }
 
 formAdd.addEventListener('submit', submitFormAdd);
